f4bd1507975030fe904ed94d67977c924452e6ca
Sam Hutchings <sam.hutchings@icloud.com>
2022-02-01 17:08:46 +0000

    Setting up the UI

--- Scoring Mars/ContentView.swift
+++ Scoring Mars/ContentView.swift
@@ -13,6 +13,29 @@ struct ContentView: View {
       NavigationView {
         VStack {
           
+          List {
+            Section(header: Text("Start New Game")) {
+              Button(action: addPlayer) {
+                Label("New Game", systemImage: "plus.circle.fill")
+              }
+            }
+            
+            Section(header: Text("Game History")) {
+              
+              NavigationLink(destination: PlayerView()) {
+                Label {
+                  Text("Game #154")
+                  
+                } icon: {
+                  Image(systemName: "globe")
+                }
+                .badge("x players")
+              }
+              
+            }
+          }
+          .listStyle(.insetGrouped)
+          
           Text("Start New Game")
           
           // button goes here
