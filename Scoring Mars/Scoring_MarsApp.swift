0c75f212efb343275b16485f7592743e3acd7262
Sam Hutchings <sam.hutchings@icloud.com>
2022-02-01 15:15:53 +0000

    Initial Commit

---
+++ Scoring Mars/Scoring_MarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scoring_MarsApp.swift
+//  Scoring Mars
+//
+//  Created by Sam Hutchings on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Scoring_MarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
